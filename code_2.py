280d2f3713f6c2a988c15b93b57c8867dd69f5a4
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2024-04-03 04:15:27 -0400

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,41 @@
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+public class RandomDataGenerator {
+
+    }
+        int[] data = new int[10];
+        }
+            System.out.println("Random Number: " + item);
+}
+        for (int item : data) {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {    });
+    const data = [];
+
+    }
+
+        console.log("Random Number: " + item);
+    return data;
+    data.forEach(item => {
+function generateRandomData() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+function main() {
+}
+    const data = generateRandomData();
+main();
+    public static void main(String[] args) {
+        }
+
+
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+    }
+        }
+public class RandomDataGenerator {
+}
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
